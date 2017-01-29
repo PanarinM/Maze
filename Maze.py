9d9b760af741d0cfbc1a947d0663a6cb67981adf
PanarinM <michaelpanarin@inbox.ru>
2017-01-29 10:01:10 +0200

    Refactored existing code

--- Maze.py
+++ Maze.py
@@ -1,151 +1,132 @@
 from random import choice
-from sys import setrecursionlimit
 from time import sleep
 from tkinter import *
 
-rectangles = []
-stack = []
-
 
 class Rectangle(object):
-    def __init__(self, width, height, number, row_count, col_count):
-        self.number = number
-        self.rows = row_count
-        self.columns = col_count
-        #            [left, right, top, bot]
-        self.walls = [True, True, True, True]
-        self.row = int(number/col_count)
-        self.column = number % col_count
+    def __init__(self, width, height, x, y, index, canvas):
+        self.canvas = canvas
         self.width = width
         self.height = height
-        self.active = False
-        self.x = 0
-        self.y = 0
-
-    def draw(self, canvas, x, y, fill='white'):
         self.x = x
         self.y = y
-        canvas.create_rectangle(x, y, x + self.width, y + self.height, fill=fill, outline=fill)
+        self.index = index
+        #            [left, right, top, bot]
+        self.walls = [True, True, True, True]
+        self.virgin = True
+        self.col = self.x / self.width
+        self.row = self.y / self.height
+
+    # def get_row_col(self):
+    #     row = self.x / self.width
+    #     col = self.y / self.height
+    #     return row, col
+
+    def draw(self, fill='white'):
+        self.canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill=fill, outline=fill)
         # top
         if self.walls[2]:
-            canvas. create_line(x, y, x + self.width, y)
+            self.canvas.create_line(self.x, self.y, self.x + self.width, self.y)
         # right
         if self.walls[1]:
-            canvas.create_line(x + self.width, y, x + self.width, y + self.height)
+            self.canvas.create_line(self.x + self.width, self.y, self.x + self.width, self.y + self.height)
         # bottom
         if self.walls[3]:
-            canvas.create_line(x + self.height, y + self.width, x, y + self.height)
+            self.canvas.create_line(self.x + self.width, self.y + self.height, self.x, self.y + self.height)
         # left
         if self.walls[0]:
-            canvas.create_line(x, y + self.height, x, y)
+            self.canvas.create_line(self.x, self.y + self.height, self.x, self.y)
 
-    def move(self, list_or_rect, canvas):
-        sleep(0.05)  # final
-        # sleep(0.5)  # debug
-        possibility = []
-        # self.draw(canvas, self.x, self.y, fill='yellow')
-        self.active = True
-        canvas.update()
+    def step(self, list_of_rect, row_c):
+        sleep(0.05)
+        ways = {}
+        self.canvas.update()
+        self.virgin = False
         try:
-            index = self.number - 1
-            if int(index / self.columns) == self.row:
-                if not index < 0:
-                    if not list_or_rect[index].active:
-                        possibility.append(list_or_rect[index])
+            if list_of_rect[self.index - 1].row == list_of_rect[self.index].row and (self.index - 1) > 0:
+                if list_of_rect[self.index - 1].virgin:
+                    ways.setdefault('01', list_of_rect[self.index - 1])
         except IndexError:
             pass
 
         try:
-            index = self.number + 1
-            if int(index / self.columns) == self.row:
-                if not list_or_rect[index].active:
-                    possibility.append(list_or_rect[index])
+            if list_of_rect[self.index + 1].row == list_of_rect[self.index].row:
+                if list_of_rect[self.index + 1].virgin:
+                    ways.setdefault('10', list_of_rect[self.index + 1])
         except IndexError:
             pass
 
         try:
-            index = self.number - self.rows
-            if not index < 0:
-                if not list_or_rect[index].active:
-                    possibility.append(list_or_rect[index])
+            if list_of_rect[self.index - row_c].col == list_of_rect[self.index].col and (self.index - row_c) > 0:
+                if list_of_rect[self.index - row_c].virgin:
+                    ways.setdefault('23', list_of_rect[self.index - row_c])
         except IndexError:
             pass
 
         try:
-            index = self.number + self.rows
-            if not list_or_rect[index].active:
-                possibility.append(list_or_rect[index])
+            if list_of_rect[self.index + row_c].col == list_of_rect[self.index].col:
+                if list_of_rect[self.index + row_c].virgin:
+                    ways.setdefault('32', list_of_rect[self.index + row_c])
         except IndexError:
             pass
 
-        if len(possibility) > 0:
-            next_num = choice(possibility)
-
-            # tut nujno ubrat nujniye steni
-            if next_num.number - self.number == 1:
-                self.walls[1] = False
-                list_or_rect[next_num.number].walls[0] = False
-
-            if next_num.number - self.number == -1:
-                self.walls[0] = False
-                list_or_rect[next_num.number].walls[1] = False
-
-            if next_num.number - self.number == self.columns:
-                self.walls[3] = False
-                list_or_rect[next_num.number].walls[2] = False
-
-            if next_num.number - self.number == -self.columns:
-                self.walls[2] = False
-                list_or_rect[next_num.number].walls[3] = False
-
-            self.draw(canvas, self.x, self.y, fill='yellow')
-            return next_num.number
+        if len(ways) > 0:
+            way = choice(list(ways.keys()))
+            next_rect = ways[way]
+            next_rect.walls[int(way[1])] = False
+            self.walls[int(way[0])] = False
+            self.draw('yellow')
+            return next_rect.index
         else:
-            self.draw(canvas, self.x, self.y, fill='yellow')
+            self.draw('yellow')
             return -1
 
 
-def gen_rects(canvas, width, height):
-    canvas.update()
-    rec_c = int((canvas.winfo_width()-2)/width)
-    rec_r = int((canvas.winfo_height()-2)/height)
-    global rectangles
-    rectangles = [Rectangle(width, height, i, rec_r, rec_c) for i in range(rec_r * rec_c)]
-    for row in range(rec_r):
-        for col in range(rec_c):
-            x = width*col+2
-            y = height*row+2
-            rectangles[row*rec_r+col].draw(canvas, x, y)
+class MazeCreator(object):
+    list_of_rects = []
+    stack = []
 
+    def __init__(self, width, height, canvas_width=802, canvas_height=802):
+        master = Tk()
+        self.canvas = Canvas(master,
+                             width=canvas_width,
+                             height=canvas_height)
+        self.canvas.pack()
+        self.width = width
+        self.height = height
+        self.canvas.update()
+        self.cols = int((self.canvas.winfo_width() - 2) / self.width)
+        self.rows = int((self.canvas.winfo_height() - 2) / self.height)
 
-def run(canvas, start, stack, rectangles):
-    index = start
-    while index >= 0:
-        stack.append(index)
-        index = rectangles[index].move(rectangles, canvas)
-    # print('stuck!')
-    if len(stack) > 1:
-        backtrack(stack, canvas, rectangles)
+    def gen_rects(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.list_of_rects.append(
+                    Rectangle(self.width, self.height, self.width * col + 2, self.height * row + 2,
+                              col + row * self.cols, self.canvas))
+                self.list_of_rects[col + row * self.cols].draw()
+        self.canvas.update()
 
+    def run(self, start):
+        index = start
+        while index >= 0:
+            self.stack.append(index)
+            index = self.list_of_rects[index].step(self.list_of_rects, self.cols)
 
-def backtrack(stack, canvas, rectangles):
-    new_index = stack.pop()
-    new_index = stack.pop()
-    rectangles[new_index].draw(canvas, rectangles[new_index].x, rectangles[new_index].y, fill='magenta')
-    run(canvas, new_index, stack, rectangles)
+    def backtrack(self):
+        new_index = self.stack.pop()
+        new_index = self.stack.pop()
+        if len(self.stack) == 0:
+            return None
+        self.list_of_rects[new_index].draw(fill='magenta')
+        return new_index
 
 
 if __name__ == '__main__':
-    setrecursionlimit(8000)
-    master = Tk()
-    canvas_width = 802
-    canvas_height = 802
-    w = Canvas(master,
-               width=canvas_width,
-               height=canvas_height)
-
-    w.pack()
-    gen_rects(w, 40, 40)
-    w.update()
-    run(w, 0, stack, rectangles)
+    maze = MazeCreator(40, 40, 802, 802)
+    maze.gen_rects()
+    index = 0
+    while index is not None:
+        maze.run(index)
+        index = maze.backtrack()
     mainloop()
